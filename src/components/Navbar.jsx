5d6da655e0b578c19c10968551423d7771acde65
malino <fazadandyakmalino0117@gmail.com>
2024-05-03 19:40:53 +0700

    add total item icon in cart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FaBagShopping, FaCompass, FaHeart } from "react-icons/fa6";
+import { StoreContext } from "../context/StoreContext";
 
 const Navbar = () => {
+  const { cartItems } = useContext(StoreContext);
+  const totalItemInCart = Object.keys(cartItems).length;
+
   const [active, setActive] = useState("explore");
   return (
     <nav className="fixed bottom-0 left-0 right-0 z-50 mx-auto h-[74px] max-w-[425px] bg-white px-7">
@@ -15,10 +19,19 @@ const Navbar = () => {
           </NavLink>
         </li>
         <li>
-          <NavLink to="shop" onClick={() => setActive("shop")}>
+          <NavLink
+            className="relative"
+            to="shop"
+            onClick={() => setActive("shop")}
+          >
             <FaBagShopping
               className={`relative bottom-1 text-4xl ${active === "shop" ? "text-orange" : "text-grey"}`}
             />
+            <div
+              className={`absolute -top-2 left-8 ${totalItemInCart >= 1 ? "flex" : "hidden"} h-4 w-max items-center justify-center rounded-md bg-orange px-1 py-1 text-xs font-medium text-white`}
+            >
+              {totalItemInCart}
+            </div>
           </NavLink>
         </li>
         <li>
